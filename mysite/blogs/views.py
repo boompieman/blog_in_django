ee24b1cd5c165b8e64c7d6f59d1d9baad18db841
Sam Chang <t0915290092@gmail.com>
2015-08-09 10:58:23 +0800

    first commit

---
+++ mysite/blogs/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render_to_response
+from blogs.models import Post
+
+def home(request):
+    post_list = Post.objects.all()
+    return render_to_response('home.html', locals())
+
+def post_detail(request, id):
+    post = Post.objects.get(id=id)
+    return render(request, 'post.html', {'post': post})
